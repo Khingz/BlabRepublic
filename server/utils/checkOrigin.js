8c8ce1dc67383a4a000b6d554d613fc526e3664e
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-06-15 17:19:51 +0100

    fix: fixed export default breaking

--- server/utils/checkOrigin.js
+++ server/utils/checkOrigin.js
@@ -1,8 +1,12 @@
-export const checkOrigin = (allowedOrigins) => (origin, callback) => {
+const checkOrigin = (allowedOrigins) => (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       callback(new Error(msg), false);
     }
-  };
+};
+
+module.exports = {
+    checkOrigin
+};
