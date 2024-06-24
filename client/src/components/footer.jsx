9a079450216c5bc72b9041161b1669ddc55fe4bd
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-06-24 18:08:45 +0100

    reftr: removed sticked to bottom for footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='mt-16'>
-      <div className='fixed bottom-0 h-12 w-full text-white text-lg md:text-xl flex items-center justify-center border-t border-white bg-gray-800'>
+      <div className='h-12 w-full text-white text-lg md:text-xl flex items-center justify-center border-t border-white bg-gray-800'>
         <p className='font-light'>All rights reserved &#169;2024 - BlabReublic</p>
       </div>
     </div>
