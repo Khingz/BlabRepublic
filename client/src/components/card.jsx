371e52426b1d4712648670a61ac993c82adfbea9
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-06-25 14:46:30 +0100

    style: post card margins and padding

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -10,18 +10,18 @@ const Card = ({ image, title, content, category, date, id }) => {
       <div className='w-full h-72 relative'>
         <img className="w-full h-full object-cover" src={image} alt={id} />
       </div>
-      <div className='flex items-center justify-start gap-2 font-semibold my-2 mx-1'>
+      <div className='flex items-center justify-start gap-2 font-semibold my-2 mx-3'>
         <p className={`${getCategoryColor(category.toLowerCase())}`}>{category || 'General'}</p>
         <p className='text-gray-800'>|</p>
         <p className='text-gray-800'>{formattedDate}</p>
       </div>
-      <div className="font-medium text-xl mx-1">
+      <div className="font-medium text-xl mx-3">
         <h3>{title.slice(0, 90)}</h3>
       </div>
       <div>
-        <p className="text-gray-700 text-base mx-1">{`${content.slice(0, 300)}...`}</p>
+        <p className="text-gray-700 text-base mx-3">{`${content.slice(0, 300)}...`}</p>
       </div>
-      <div className='mt-4 mb-3 mx-1'>
+      <div className='mt-4 mb-6 mx-2'>
         <Link to={`/posts/${id}`} className='bg-none border border-black text-black hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white text-center rounded-sm px-3 py-2'>Read more</Link>
       </div>
     </div>
